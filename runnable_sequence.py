c507b1da8c13f396622de0101458f30970b92767
uneiz1103 <uneizshaikh1103@gmail.com>
2025-07-31 17:31:18 +0530

    updated

--- runnable_sequence.py
+++ runnable_sequence.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain.schema.runnable import RunnableSequence
+from langchain_core.runnables import RunnableSequence
 from dotenv import load_dotenv
 
 

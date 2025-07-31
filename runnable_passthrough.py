58f475c3eac209f196deb34f8ecf5e3a6090b100
uneiz1103 <uneizshaikh1103@gmail.com>
2025-07-31 15:21:55 +0530

    Add runnable_passthrough.py with joke generation and explanation using RunnablePassthrough

--- runnable_passthrough.py
+++ runnable_passthrough.py
@@ -1,11 +1,32 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough, RunnableSequence, RunnableParallel
+from langchain_core.runnables import RunnableParallel, RunnableSequence, RunnablePassthrough
 from dotenv import load_dotenv
 
 load_dotenv()
 
-passthrough = RunnablePassthrough()
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
 
-print(passthrough.invoke({'name': 'uneiz'}))
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following Joke - {text}',
+    input_variables=['text']
+)
+
+joke_gen_chain = RunnableSequence(prompt1, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'explanation': (prompt2, model, parser)
+})
+
+final_chain = RunnableSequence(joke_gen_chain , parallel_chain)
+
+print(final_chain.invoke({'topic':'football'}))
